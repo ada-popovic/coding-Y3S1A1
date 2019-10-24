0446b15be9415868fa2084bddc7b19a77c6b1d3e
ada popovic <popowucz.adrianna@gmail.com>
2019-10-24 20:15:55 +0200

    --

---
+++ script.js
@@ -0,0 +1,12 @@
+var txtFile = new XMLHttpRequest();
+    var allText = "file not found";
+    txtFile.onreadystatechange = function () {
+        if (txtFile.readyState === XMLHttpRequest.DONE && txtFile.status == 200) {
+            allText = txtFile.responseText;
+            allText = allText.split("\n").join("<br>");
+        }
+
+        document.getElementById('text').innerHTML = allText;
+    }
+    txtFile.open("GET", 'guten_address.txt', true);
+    txtFile.send(null);
