c5301c099d5ff35c2fd70c8965d40962a3f4b9a1
ada popovic <popowucz.adrianna@gmail.com>
2019-10-24 23:01:11 +0200

    --

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function load() {
-    var file = new XMLHttpRequest();
-    file.open("GET", "guten_address.txt", true);
-    file.onreadystatechange = function() {
-      if (file.readyState === 4) {  // Makes sure the document is ready to parse
-        if (file.status === 200) {  // Makes sure it's found the file
-          text = file.responseText;
-          document.getElementById("text").innerHTML = text;
-        }
-      }
-    }
-}
-
-window.onLoad = load();
+// function load() {
+//     var file = new XMLHttpRequest();
+//     file.open("GET", "guten_address.txt", true);
+//     file.onreadystatechange = function() {
+//       if (file.readyState === 4) {  // Makes sure the document is ready to parse
+//         if (file.status === 200) {  // Makes sure it's found the file
+//           text = file.responseText;
+//           document.getElementById("text").innerHTML = text;
+//         }
+//       }
+//     }
+// }
+//
+// window.onLoad = load();
