6f2c285125c38c7766982d657441b5d3e61a8c23
ada popovic <popowucz.adrianna@gmail.com>
2019-11-04 15:30:35 +0100

    loading text

---
+++ sketch.js
@@ -0,0 +1,65 @@
+// var txt;
+// var counts = {};
+// var keys = [];
+//
+// function preload() {
+//   txt = loadStrings('files/rainbow.html');
+//
+// }
+//
+// function setup() {
+//   var allwords = txt.join("\n");
+//   var tokens = allwords.split(/\W+/);
+//   for (var i = 0; i < tokens.length; i++) {
+//     var word = tokens[i].toLowerCase();
+//     if (!/\d+/.test(word)) {
+//       //console.log(word);
+//       if (counts[word] === undefined) {
+//         counts[word] = 1;
+//         keys.push(word);
+//       } else {
+//         counts[word] = counts[word] + 1;
+//       }
+//     }
+//   }
+//
+//   keys.sort(compare);
+//
+//   function compare(a, b) {
+//     var countA = counts[a];
+//     var countB = counts[b];
+//     return countB - countA;
+//   }
+//
+//   for (var i = 0; i < keys.length; i++) {
+//     var key = keys[i];
+//     createDiv(key + " " + counts[key]);
+//   }
+//
+//
+//
+//   noCanvas();
+// }
+
+
+
+
+
+
+$(document).ready(function(){
+
+   $('#content').load("98-story.html");
+
+});
+
+
+
+
+
+// var fs = require('fs');
+//
+// fs.readFile('rainbow.txt', function (err, data) {
+//                     if (err) throw err;
+//
+//     console.log(data);
+// });
