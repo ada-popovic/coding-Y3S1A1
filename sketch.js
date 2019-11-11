25c163944cb061e4594a80ea5dba387841ff9d06
ada popovic <popowucz.adrianna@gmail.com>
2019-11-11 14:22:59 +0100

    --

--- sketch.js
+++ sketch.js
@@ -41,54 +41,71 @@
 //   noCanvas();
 // }
 
-$(document).ready(function(){
-
-   $('#content').load("8-story.html");
-
-});
+// $(document).ready(function(){
+//
+//    $('#content').load("8-story.html");
+//
+// });
 
 ////////////
 
 
+// var txt;
+// var counts = {};
+// var keys = [];
+//
+// function preload() {
+//   txt = loadStrings("8-story.txt");
+//
+// }
+//
+// function setup() {
+//   var allwords = txt.join("\n");
+//   var tokens = allwords.split(/\W+/);
+//   for (var i = 0; i < tokens.length; i++) {
+//     var word = tokens[i].toLowerCase();
+//     if (!/\d+/.test(word)) {
+//       //console.log(word);
+//       if (counts[word] === undefined) {
+//         counts[word] = 1;
+//         keys.push(word);
+//       } else {
+//         counts[word] = counts[word] + 1;
+//       }
+//     }
+//   }
+//
+//   keys.sort(compare);
+//
+//   function compare(a, b) {
+//     var countA = counts[a];
+//     var countB = counts[b];
+//     return countB - countA;
+//   }
+//
+//   for (var i = 0; i < keys.length; i++) {
+//     var key = keys[i];
+//     createDiv(key + " " + counts[key]);
+//   }
+//
+//
+//
+//   noCanvas();
+// }
+//////////
+
+
 var txt;
-var counts = {};
-var keys = [];
 
 function preload() {
-  txt = loadStrings("8-story.txt");
+  txt= loadStrings('8-story.html');
 
 }
 
 function setup() {
-  var allwords = txt.join("\n");
-  var tokens = allwords.split(/\W+/);
-  for (var i = 0; i < tokens.length; i++) {
-    var word = tokens[i].toLowerCase();
-    if (!/\d+/.test(word)) {
-      //console.log(word);
-      if (counts[word] === undefined) {
-        counts[word] = 1;
-        keys.push(word);
-      } else {
-        counts[word] = counts[word] + 1;
-      }
-    }
-  }
-
-  keys.sort(compare);
-
-  function compare(a, b) {
-    var countA = counts[a];
-    var countB = counts[b];
-    return countB - countA;
-  }
-
-  for (var i = 0; i < keys.length; i++) {
-    var key = keys[i];
-    createDiv(key + " " + counts[key]);
-  }
-
-
+  var allwords = txt.join('\n');
+  var tokens = allwords.split(/\W+);
+  console.log(tokens);
 
   noCanvas();
 }
