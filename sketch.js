fbcdac683f0bcb7479b6707a61ed4ae24e315d1b
ada popovic <popowucz.adrianna@gmail.com>
2019-11-11 15:06:06 +0100

    overlap

--- sketch.js
+++ sketch.js
@@ -48,51 +48,68 @@
 // });
 
 ////////////
+var overlap = require('word-overlap');
 
+var sentence1 = 'The Hitchhikings Meetup in Betelgeuse by Ford Prefect';
+var sentence2 = 'The hitchhikings meetups by the hitchhikers';
 
-var txt;
-var counts = {};
-var keys = [];
+var reply = overlap(sentence1, sentence2, {
+  ignoreCase: true,
+  minWordLength: 2,
+  ignoreCommonWords: true
+});
 
-function preload() {
-  txt = loadStrings("5-story.html");
-
-}
-
-function setup() {
-  var allwords = txt.join("\n");
-  var tokens = allwords.split(/\W+/);
-  console.log(tokens);
-  for (var i = 0; i < tokens.length; i++) {
-    var word = tokens[i].toLowerCase();
-    if (!/\d+/.test(word)) {
-      //console.log(word);
-      if (counts[word] === undefined) {
-        counts[word] = 1;
-        keys.push(word);
-      } else {
-        counts[word] = counts[word] + 1;
-      }
-    }
-  }
-
-  keys.sort(compare);
-
-  function compare(a, b) {
-    var countA = counts[a];
-    var countB = counts[b];
-    return countB - countA;
-  }
-
-  for (var i = 0; i < keys.length; i++) {
-    var key = keys[i];
-    createDiv(key + " " + counts[key]);
-  }
+console.log(reply);
 
 
 
-  noCanvas();
-}
+
+
+
+
+// var txt;
+// var counts = {};
+// var keys = [];
+//
+// function preload() {
+//   txt = loadStrings("5-story.html");
+//
+// }
+//
+// function setup() {
+//   var allwords = txt.join("\n");
+//   var tokens = allwords.split(/\W+/);
+//   console.log(tokens);
+//   for (var i = 0; i < tokens.length; i++) {
+//     var word = tokens[i].toLowerCase();
+//     if (!/\d+/.test(word)) {
+//       //console.log(word);
+//       if (counts[word] === undefined) {
+//         counts[word] = 1;
+//         keys.push(word);
+//       } else {
+//         counts[word] = counts[word] + 1;
+//       }
+//     }
+//   }
+//
+//   keys.sort(compare);
+//
+//   function compare(a, b) {
+//     var countA = counts[a];
+//     var countB = counts[b];
+//     return countB - countA;
+//   }
+//
+//   for (var i = 0; i < keys.length; i++) {
+//     var key = keys[i];
+//     createDiv(key + " " + counts[key]);
+//   }
+//
+//
+//
+//   noCanvas();
+// }
 //////////
 
 
